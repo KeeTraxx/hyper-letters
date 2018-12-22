fa2a7a7848e53265a30db4c71fa07eb2d58998de
Khôi Tran <kt@compile.ch>
2018-12-22 14:23:40 +0100

    compatiblity with windows

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-let letter;
-
-let active = false;
-
-let activeUi
-
-exports.middleware = (store) => (next) => (action) => {
-  const {sessions, termGroups, ui} = store.getState()
-
-  switch(action.type) {
-    case 'SESSION_ADD':
-    case 'SESSION_SET_ACTIVE':
-      activeUi = ui
-      break
-    case 'SESSION_USER_DATA':
-      break
-    default:
-      letter = action.data
-      break
-  }
-  next(action)
-};
-
 exports.decorateTerm = (Term, { React, notify }) => {
   return class extends React.Component {
     constructor (props, context) {
@@ -46,35 +23,35 @@ exports.decorateTerm = (Term, { React, notify }) => {
 
       // Hold array of letters to handle in _drawFrame
       this._letters = [];
+      this._active = false;
     }
 
-    _spawnLetter (x, y) {
-      // Only spawn letter when it is 1 printable character
-      if (letter && letter.match(/^[\u0020-\u007e\u00a0-\u00ff]$/)) {
-        const length = this._letters.length;
+    _spawnLetter (letter, x, y) {
+      const length = this._letters.length;
 
-        // Initial values
-        this._letters.push({
-          // Character in question
-          letter,
-          // position
-          x,
-          y,
-          // rotation
-          rot: 0,
-          // velocity
-          vx: Math.random() - 0.5,
-          vy: -1,
-          // radial velocity
-          vr: (Math.random() - 0.5) * 0.2,
-          // time to live
-          ttl: 500
-        });
+      // Initial values
+      this._letters.push({
+        // Character in question
+        letter,
+        // position
+        x,
+        y,
+        // rotation
+        rot: 0,
+        // velocity
+        vx: (Math.random() - 0.5) * 0.5,
+        vy: -0.4,
+        // radial velocity
+        vr: (Math.random() - 0.5) * 0.1,
+        // time to live
+        ttl: 1000,
+        lastBounce: 0
+      });
 
-        if (!active) {
-          window.requestAnimationFrame(this._drawFrame);
-        }  
-      }
+      if (!this._active) {
+        window.requestAnimationFrame(this._drawFrame);
+        this._active = true
+      }  
     }
 
     _onDecorated (term) {
@@ -92,18 +69,37 @@ exports.decorateTerm = (Term, { React, notify }) => {
       this._canvas.width = window.innerWidth;
       this._canvas.height = window.innerHeight;
       document.body.appendChild(this._canvas);
-      window.requestAnimationFrame(this._drawFrame);
+      // window.requestAnimationFrame(this._drawFrame);
       window.addEventListener('resize', this._resizeCanvas);
+      window.addEventListener('keydown', (ev) => {
+        if (ev.key.match(/^[\u0020-\u007e\u00a0-\u00ff]$/)) {
+          let origin = this._div.getBoundingClientRect();
+          let modifiers = []
+          ev.ctrlKey && modifiers.push('Ctrl')
+          this._spawnLetter((ev.ctrlKey ? '^' : '') + ev.key, this._cursorPosition.x + origin.left, this._cursorPosition.y + origin.top)
+        }
+      })
     }
 
-    _drawFrame () {
-      this._canvasContext.clearRect(0, 0, this._canvas.width, this._canvas.height);
+    _drawFrame (time) {
+      if (!this._lastRender) {
+        this._lastRender = time
+        window.requestAnimationFrame(this._drawFrame);
+        return
+      }
+      let dt = time - this.lastRender
+      this.lastRender = time
 
+      console.log(dt)
+      
+      // this._canvasContext.clearRect(0, 0, this._canvas.width, this._canvas.height);
+      // this._canvasContext.fillStyle = 'rgba(255,0,0,0.4)'
+      this._canvasContext.clearRect(0, 0, this._canvas.width, this._canvas.height);
+      // this._canvasContext.fillRect(0, 0, this._canvas.width, this._canvas.height - 100);
       // handle each letter
       this._letters.forEach(l => {
-
         // when the character bounces off bottom
-        if (l.y > this._canvas.height - 10) {
+        if (l.lastBounce > 10 && l.y > this._canvas.height - 10) {
           // bouncyness
           l.vy *= -0.6
 
@@ -112,7 +108,10 @@ exports.decorateTerm = (Term, { React, notify }) => {
 
           // dampen horizontal speed
           l.vx *= 0.8
+          l.lastBounce = 0
         }
+
+        l.lastBounce++
 
         // TODO: Maybe handle left / right walls too?
 
@@ -120,32 +119,32 @@ exports.decorateTerm = (Term, { React, notify }) => {
         l.vy += 0.02
 
         // physics
-        l.x += l.vx
-        l.y += l.vy
+        l.x += l.vx * dt
+        l.y += l.vy * dt
 
         // rotation
-        l.rot += l.vr
+        l.rot += l.vr * dt
         l.ttl--
 
         this._canvasContext.save();
         this._canvasContext.translate(l.x, l.y);
         this._canvasContext.rotate(l.rot);
-        this._canvasContext.font = `${activeUi.fontSize}pt ${activeUi.fontFamily}`;
-        this._canvasContext.fillStyle = activeUi.foregroundColor;
+        this._canvasContext.font = `${this.props.fontSize}pt ${this.props.fontFamily}`;
+        this._canvasContext.fillStyle = this.props.foregroundColor;
         this._canvasContext.textAlign = 'center';
         this._canvasContext.fillText(l.letter, 0, 0);
         this._canvasContext.restore();
       })
 
-      // remove letter when TTL is 0
-      this._letters = this._letters.filter(l => l.ttl > 0);
-
       // request a new animation frame when there are still letters left
       if (this._letters.length > 0) {
         window.requestAnimationFrame(this._drawFrame);
       } else {
-        active = false;
+        this._active = false;
       }
+
+      // remove letter when TTL is 0
+      this._letters = this._letters.filter(l => l.ttl > 0);
     }
 
     _resizeCanvas () {
@@ -157,14 +156,15 @@ exports.decorateTerm = (Term, { React, notify }) => {
       if (this.props.onCursorMove) this.props.onCursorMove(cursorFrame);
       
       const { x, y } = cursorFrame;
-      const origin = this._div.getBoundingClientRect();
-      requestAnimationFrame(() => {
-        this._spawnLetter(x+origin.left, y+origin.top);
+      this._cursorPosition = {x , y}
+      // const origin = this._div.getBoundingClientRect();
+      /* requestAnimationFrame(() => {
+        // this._spawnLetter(x+origin.left, y+origin.top);
         if (!active) {
           active = true;
           this._drawFrame();
         }
-      })
+      })*/
     }
 
     render () {
